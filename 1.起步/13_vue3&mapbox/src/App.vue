58741a3cb30112b430037c754e6b3ff2176f5151
xianren <2958197921@qq.com>
2023-11-17 15:56:41 +0800

    add

---
+++ 1.起步/13_vue3&mapbox/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="container">
+  </div>
+</template>
+
+<script setup lang="ts">
+import mapboxGl from 'mapbox-gl'
+import { onMounted } from 'vue'
+// 获取token
+mapboxGl.accessToken = import.meta.env.VITE_TOKEN
+
+// 进行地图配置
+onMounted(()=>{
+  const map = new mapboxGl.Map({
+    container: 'container', // 容器id
+    style: 'mapbox://styles/mapbox/streets-v12', // 样式
+    center: [116.395645, 39.92998], // 中心点
+    zoom: 12 // 缩放级别
+  })
+  map.addControl(new mapboxGl.NavigationControl())
+})
+</script>
+
+<style lang="scss" scoped>
+#container{
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
+</style>
